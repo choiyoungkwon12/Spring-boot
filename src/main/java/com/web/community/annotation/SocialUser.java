80ff40070b760b602dd450694e8b40fd52539cb4
choiyoungkwon <hpwh4457@gmail.com>
2021-05-23 21:34:04 +0900

    시큐리티와 OAuth2사용하여 기본적인 인증과 권한 부여 처리

---
+++ src/main/java/com/web/community/annotation/SocialUser.java
@@ -0,0 +1,2 @@
+package com.web.community.annotation;public @interface SocialUser {
+}
--- src/main/java/com/web/community/config/SecurityConfig.java
+++ src/main/java/com/web/community/config/SecurityConfig.java
@@ -1,20 +1,41 @@
 package com.web.community.config;
 
+import com.web.community.domain.enums.SocialType;
 import com.web.community.oauth.ClientResources;
+import com.web.community.oauth.UserTokenService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter.CompositeFilter;
+
+import javax.servlet.Filter;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.web.community.domain.enums.SocialType.*;
 
 
 @Configuration
 @EnableWebSecurity
+@EnableOAuth2Client
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private OAuth2ClientContext oAuth2ClientContext;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -22,25 +43,65 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         CharacterEncodingFilter filter = new CharacterEncodingFilter();
         http.
                 authorizeRequests()
-                    .antMatchers("/", "/login/**", "/css/**", "/images/**", "/js/**", "/console/**").permitAll()
-                    .anyRequest().authenticated()
+                .antMatchers("/", "/login/**", "/css/**", "/images/**", "/js/**", "/console/**").permitAll()
+                .anyRequest().authenticated()
                 .and()
-                    .headers().frameOptions().sameOrigin() // disable() : 거부 / sameOrigin() : 동일한 도메인에서는 iframe 접근 가능
+                .headers().frameOptions().disable() // disable() : 거부 / sameOrigin() : 동일한 도메인에서는 iframe 접근 가능
                 .and()
-                    .exceptionHandling()
-                    .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"))
+                .exceptionHandling()
+                .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"))
                 .and()
-                    .formLogin()
-                    .successForwardUrl("/board/list")
+                .formLogin()
+                .successForwardUrl("/board/list")
                 .and()
-                    .logout()
-                    .logoutUrl("/logout")
-                    .logoutSuccessUrl("/")
-                    .deleteCookies("JSESSIONID")
-                    .invalidateHttpSession(true)
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/")
+                .deleteCookies("JSESSIONID")
+                .invalidateHttpSession(true)
                 .and()
-                    .addFilterBefore(filter, CsrfFilter.class)
-                    .csrf().disable();
+                .addFilterBefore(filter, CsrfFilter.class)
+                .addFilterBefore(oauth2Filter(), BasicAuthenticationFilter.class)
+                .csrf().disable();
+    }
+
+    @Bean
+    public FilterRegistrationBean oauth2ClientFilterRegistration(OAuth2ClientContextFilter filter) {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(filter);
+        registration.setOrder(-100);
+        return registration;
+    }
+
+    private Filter oauth2Filter() {
+        CompositeFilter filter = new CompositeFilter();
+        List<Filter> filters = new ArrayList<>();
+        filters.add(oauth2Filter(facebook(), "/login/facebook", FACEBOOK));
+        filters.add(oauth2Filter(google(), "/login/google", GOOGLE));
+        filters.add(oauth2Filter(kakao(), "/login/kakao", KAKAO));
+        filter.setFilters(filters);
+        return filter;
+    }
+
+    private Filter oauth2Filter(ClientResources clientResources, String path, SocialType socialType) {
+
+        //인증이 수행될 경로를 넣어 OAuth2 클라이언트용 인증처리 필터를 생성
+        OAuth2ClientAuthenticationProcessingFilter filter = new OAuth2ClientAuthenticationProcessingFilter(path);\
+
+        //권한 서버와의 통신을 위해 OAuth2RestTemplate을 생성합니다.
+        OAuth2RestTemplate template = new OAuth2RestTemplate(clientResources.getClient(), oAuth2ClientContext);
+
+        filter.setRestTemplate(template);
+
+        //User의 권한을 최적회해서 생성하고자 UserInfoTokenServices를 상속받은 UserTokenService를 생성했습니다.
+        //OAuth2 AccessToken 검증을 위해 생성한 UserTokenService를 필터의 토큰 서비스로 등록합니다.
+        filter.setTokenServices(new UserTokenService(clientResources, socialType));
+
+        // 인증이 성공적으로 이루어지면 필터에 리다이렉트될 URL을 설정합니다.
+        filter.setAuthenticationSuccessHandler((request, response, authentication) -> response.sendRedirect("/" + socialType.getValue() + "/complete"));
+        // 인증이 실패하면 이필터에 리다이렉트될 URL을 설정합니다.
+        filter.setAuthenticationFailureHandler((request, response, exception) -> response.sendRedirect("/error"));
+        return filter;
     }
 
     @Bean
