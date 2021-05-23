fa0e9ca368fa2ae8613d870841b67e7848ef504f
choiyoungkwon <hpwh4457@gmail.com>
2021-05-23 22:34:49 +0900

    사용자 인증 처리

--- src/main/java/com/web/community/annotation/SocialUser.java
+++ src/main/java/com/web/community/annotation/SocialUser.java
@@ -1,2 +1,11 @@
-package com.web.community.annotation;public @interface SocialUser {
+package com.web.community.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface SocialUser {
 }
--- src/main/java/com/web/community/controller/LoginController.java
+++ src/main/java/com/web/community/controller/LoginController.java
@@ -1,2 +1,36 @@
-package com.web.community.controller;public class LoginController {
+package com.web.community.controller;
+
+import com.web.community.annotation.SocialUser;
+import com.web.community.domain.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public String login() {
+        return login();
+    }
+
+    @GetMapping(value = "/{facebook|kakao|google}/complete")
+    public String loginComplete(@SocialUser User user) {
+        return "redirect:/board/list";
+    }
+    /*@GetMapping(value = "/{facebook|kakao|google}/complete")
+    public String loginComplete(HttpSession session) {
+        OAuth2Authentication authentication = (OAuth2Authentication) SecurityContextHolder.getContext().getAuthentication();
+
+        Map<String, String> map = (Map<String, String>) authentication.getUserAuthentication().getDetails();
+
+        session.setAttribute("user", User.builder()
+                .name(map.get("name"))
+                .email(map.get("email"))
+                .principal(map.get("id"))
+                .socialType(SocialType.FACEBOOK)
+                .createdDate(LocalDateTime.now())
+                .build()
+        );
+        return "redirect:/board/list";
+    }*/
+
 }
